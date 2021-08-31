992b80658e8308008c4a046bc7add2821e94199e
Jan Ibañez <jwgibanez@protonmail.ch>
2021-08-31 21:05:00 +0800

    project: initial commit

---
+++ app/src/main/java/io/github/jwgibanez/stb/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.jwgibanez.stb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.jwgibanez.stb.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/io/github/jwgibanez/stb/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.jwgibanez.stb.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
