0a1b84576b6135d9cddaedd2a7cec916e0d4d8da
Jan Ibañez <jwgibanez@protonmail.ch>
2021-09-04 23:16:32 +0800

    ui: add some instructions and image resources

--- app/src/main/java/io/github/jwgibanez/stb/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/jwgibanez/stb/ui/main/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.github.jwgibanez.stb.databinding.FragmentMainBinding
 
@@ -19,6 +20,13 @@ class MainFragment : Fragment() {
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.startButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Hey!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
